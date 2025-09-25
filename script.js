22ec05c3731933fa8e232507afd8b782d674ff5b
moralvaradoyahir-dot <moralvaradoyahir@gmail.com>
2025-09-25 17:01:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ function generarDatos() {
   // Math.random() → número entre 1 y 9
   // * citas.length → escala ese número al rango de datos disponibles
   // Math.floor() → redondea hacia abajo para obtener un dato válido
-  const indice = datos.floor(datos.random() * datos.length);
+  const indice = Math.floor(Math.random() * datos.length);
 
   // Buscamos en el documento el elemento con id="quote"
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
   document.getElementById("quote").innerText = datos[indice];
 }
+
 
 
 

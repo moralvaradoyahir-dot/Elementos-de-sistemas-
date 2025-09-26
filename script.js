3c08620ab98280d17354e0a6bc01652477e04200
moralvaradoyahir-dot <moralvaradoyahir@gmail.com>
2025-09-26 09:15:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Declaramos un arreglo (array) con varios platillos sobre el Generador de antojitos mexicanos 
-const Generador Antojitos Mexicanos = [Generador Antojitos Mexicanos 
+// Declaramos un arreglo (array) con varios platillos sobre el Generador de antojitos mexicanos
+const antojitosMexicanos = [
   "Quesadillas.",
   "Sopes.",
   "Tamales.",
@@ -10,40 +10,23 @@ const Generador Antojitos Mexicanos = [Generador Antojitos Mexicanos
 ];
 
 // Definimos la función que mostrará un dato aleatorio
-function Generador Antojitos Mexicanos() {
-  // Generamos un dato aleatorio entre la cantidad de datos (datos.length)
-  // Math.random() → Antojitos mexicanos 
-  // * antojitos mexicanos.length → escala ese número al rango de datos disponibles
-  // Math.floor() → redondea hacia abajo para obtener un dato válido
-  const indice = Math.floor(Math.random() *AntojitosMexicanos.length);
+function generadorAntojitosMexicanos() {
+  // Generamos un dato aleatorio entre la cantidad de datos (antojitosMexicanos.length)
+  // Math.random() → Genera un número aleatorio entre 0 y 1
+  // * antojitosMexicanos.length → Escala ese número al rango de datos disponibles
+  // Math.floor() → Redondea hacia abajo para obtener un índice válido
+  const indice = Math.floor(Math.random() * antojitosMexicanos.length);
 
   // Buscamos en el documento el elemento con id="quote"
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
-  document.getElementById("quote").innerText =AntojitosMexicanos [indice];
+  document.getElementById("quote").innerText = antojitosMexicanos[indice];
 }
-"CREA UNA FUNCIÓN PARA"
-"SUMAR 2 NÚMEROS"
 
-def suma (a, b):
-    return a + b
-print(suma(10, 20))
+// Función para sumar 2 números
+function suma(a, b) {
+  return a + b;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Ejemplo de cómo llamar la función suma
+const resultado = suma(10, 20);
+console.log("La suma de 10 y 20 es: " + resultado);
